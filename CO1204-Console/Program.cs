5a64f8c14374ed9d57d1008b691aafc59f51e399
Cuong Hoang~ <hoangcuongbk80@gmail.com>
2025-01-15 11:43:17 +0700

    week 1

---
+++ CO1204-Console/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace CO1204_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Campus: ");
+            string campus = Console.ReadLine();
+
+            Console.WriteLine("Enter a year: ");
+            string year_str = Console.ReadLine();
+            int year = int.Parse(year_str);
+
+            Console.WriteLine("Hi {0} students", campus);
+            Console.Write("Happy ");
+            Console.Write("New Year {0}!", year);
+
+            Console.Read();
+        }
+    }
+}
